8ea0b8a0390733e2be5014808cc916d3043a89f1
VadimKurachynski <kurachynski@gmail.com>
2022-08-21 18:03:35 +0300

    21p

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('hello from Node.js')
+function alerted(){
+   
+    var el = document.getElementById('promptaudio');
+    el.style.display === 'none' ? el.style.display = 'initial' : el.style.display = 'none';
+  }
+
+
+  function prov(){
+    var el = document.getElementById('audio1text').value;
+    el=el.toLowerCase();
+    if (el=="открытка") {
+      var elv = document.getElementById('prompvideo');
+     elv.style.display = 'initial';
+    }else{
+      alert("НЕ ВЕРНО!");
+    }
+    
+  }
